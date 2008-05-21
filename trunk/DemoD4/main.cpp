8df1ac4ed985c1826cc37b1bd8e07f42f8d6020a
doqkhanh <doqkhanh@52f955dd-904d-0410-b1ed-9fe3d3cbfe06>
2008-05-21 02:16:48 +0000

    Edit style and add comment

--- trunk/DemoD4/main.cpp
+++ trunk/DemoD4/main.cpp
@@ -1,13 +1,26 @@
+/*
+***********************************************
+* Main entry for program
+* *********************************************
+* file name: main.cpp
+* encoding: UTF-8
+* tab size: 8
+* indentation: 4
+* created on: 6:36 PM 5/2/2008
+* init code by: _REPLACE_IN_ALL_PROJECT_ ,Irrlicht Forum
+* created by: FOSP Team
+* copyright: FOS Project
+*/
+
 #include "Application.h"
 #pragma comment(lib, "Irrlicht.lib")
-int main(int argc, char** argv)
 
-{
-	
+int main(int argc, char** argv)
+{	
 	Application app;
 
 	if (app.init())
 		app.run();
 
-   return 0;
+	return 0;
 }
