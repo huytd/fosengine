a7f03695094ce829f85d06d385d13a229e55cc9c
doqkhanh <doqkhanh@52f955dd-904d-0410-b1ed-9fe3d3cbfe06>
2008-06-15 16:50:14 +0000

    Correct wront resource name error: HP.png

--- Simple Game Framework/D3(with SGF)/Level01.cpp
+++ Simple Game Framework/D3(with SGF)/Level01.cpp
@@ -4,33 +4,36 @@ Level01::Level01()
 }
 void Level01::onEnter(sgfEntityManager* emgr)
 {
-		this->emgr = emgr;
-		env=emgr->getCore()->getGraphicDevice()->getGUIEnvironment();
-	 //HP and MP Bar.
-        HPBar = new HealthManaBar(rect<s32>(0,0,800,600),env,env->getRootGUIElement());
-		MPBar = new HealthManaBar(rect<s32>(0,0,800,600),env,env->getRootGUIElement());
-		HPBar->setTexture("bar/untitled.png");
-		HPBar->setPosition(10,10);
-		HPBar->setMaxValue(300);
-		HPBar->setValue(190);
-		MPBar->setTexture("bar/MP.png");
-		MPBar->setPosition(10,36);
-		MPBar->setMaxValue(150);
-		MPBar->setValue(110);
-		HPBar->drop();
-		MPBar->drop();
+	this->emgr = emgr;
+	env=emgr->getCore()->getGraphicDevice()->getGUIEnvironment();
+	//HP and MP Bar.
+	HPBar = new HealthManaBar(rect<s32>(0,0,800,600),env,env->getRootGUIElement());
+	MPBar = new HealthManaBar(rect<s32>(0,0,800,600),env,env->getRootGUIElement());
+	HPBar->setTexture("bar/HP.png");
+	HPBar->setPosition(10,10);
+	HPBar->setMaxValue(300);
+	HPBar->setValue(190);
+	MPBar->setTexture("bar/MP.png");
+	MPBar->setPosition(10,36);
+	MPBar->setMaxValue(150);
+	MPBar->setValue(110);
+
+	//Destroy object
+	HPBar->drop();
+	MPBar->drop();
+
 	//World.
-        emgr->getCore()->getGraphicDevice()->getSceneManager()->loadScene("levels/start.irr");
+	emgr->getCore()->getGraphicDevice()->getSceneManager()->loadScene("levels/start.irr");
 
 	//Map.
-		map = new Map(rect<s32>(0,0,800,600),env,env->getRootGUIElement());
-		map->setMapTexture("map/LevelOneState.jpg");
-		map->setCharacterTexture("map/character.jpg");
-		map->setWorldSize(10000,10000);
-		map->setCharPosition(core::vector3df(5000,0,5000));
-		map->drop();
+	map = new Map(rect<s32>(0,0,800,600),env,env->getRootGUIElement());
+	map->setMapTexture("map/LevelOneState.jpg");
+	map->setCharacterTexture("map/character.jpg");
+	map->setWorldSize(10000,10000);
+	map->setCharPosition(core::vector3df(5000,0,5000));
+	map->drop();
 }
 void Level01::onExit(sgfEntityManager* emgr)
 {
-		env->clear();
+	env->clear();
 }
