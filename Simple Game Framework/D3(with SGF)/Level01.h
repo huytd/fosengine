2e08adbea219f7adaea552e8d8c3138c453fdc21
kingbazoka <kingbazoka@52f955dd-904d-0410-b1ed-9fe3d3cbfe06>
2008-06-18 04:44:51 +0000

    -Game Toolbar -IGUIButton -Unicode font

--- Simple Game Framework/D3(with SGF)/Level01.h
+++ Simple Game Framework/D3(with SGF)/Level01.h
@@ -4,6 +4,7 @@
 #include <SGF.h>
 #include "HealthManaBar.h"
 #include "Map.h"
+
 class Level01:public sgfIrrLevel
 {
 private:
@@ -12,6 +13,11 @@ private:
 	HealthManaBar* HPBar;
 	HealthManaBar* MPBar;
 	Map* map;
+	irr::gui::IGUIImage* toolbar;
+	irr::gui::IGUIButton* btn1;
+	irr::gui::IGUIButton* btn2;
+	irr::gui::IGUIButton* btn3;
+	sgfMethodDelegate<Level01,irr::SEvent::SGUIEvent> onGUI;
 
 public:
 
@@ -19,5 +25,6 @@ public:
 	void onEnter(sgfEntityManager* emgr);
 	void onExit(sgfEntityManager* emgr);
 	void onUpdate(sgfEntityManager* emgr);
+	void onGUIEvent(irr::SEvent::SGUIEvent& args);
 };
 #endif
