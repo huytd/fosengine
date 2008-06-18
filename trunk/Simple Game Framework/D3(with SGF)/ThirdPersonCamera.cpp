ff7361f98e82a958de33706db46176a15916394a
doqkhanh <doqkhanh@52f955dd-904d-0410-b1ed-9fe3d3cbfe06>
2008-06-18 08:13:11 +0000

    New Camera by Giang, commit by Khanh

--- trunk/Simple Game Framework/D3(with SGF)/ThirdPersonCamera.cpp
+++ trunk/Simple Game Framework/D3(with SGF)/ThirdPersonCamera.cpp
@@ -1,4 +1,14 @@
-﻿#include "ThirdPersonCamera.h"
+﻿/**
+ * \RunOn: SGF(Simple Game Framework) - Irrlicht Engine
+ * \Summary: Third Person Camera
+ * \Filename: ThirdPersonCamera.cpp
+ * \Encoding: UTF-8
+ * \CreatedDate: 2:17:PM 2008/06/15
+ * \CreatedBy: FOSP Team - NamVoDang(kiemkhachchilo)
+ * \Copyright: FOS Project
+ **/
+
+#include "ThirdPersonCamera.h"
 using namespace irr;
 using namespace scene;
 using namespace core;
@@ -6,10 +16,12 @@ ThirdPersonCamera::ThirdPersonCamera(ISceneNode* targetNode)
 {    
 	    this->targetNode = targetNode;
 		Zoom = 2;
+		nRotationX = 0;
+		nRotationY = 0;
 		rotating = false;
 		Mouse[0] = false;
 		Mouse[1] = false;
-		rotation = vector3df(0,15,-25);//Xoay mặc định khi vào game.
+		rotation = vector3df(0,15,-25);//Default rotation on level enter.
 		mouseDelegate.addRef();
 		mouseDelegate.bind(this,&ThirdPersonCamera::onMouse);
 } 
@@ -18,8 +30,8 @@ void ThirdPersonCamera::onAdd()
 {
 		cursor = manager->getCore()->getGraphicDevice()->getCursorControl();
 		ISceneManager* smgr = manager->getCore()->getGraphicDevice()->getSceneManager();
-		camera = smgr->addCameraSceneNode();//Thêm 1 camera.
-	 //Lấy terrain.
+		camera = smgr->addCameraSceneNode();//add camera.
+	 //terrain.
 		terrain = manager->getCore()->globalVars["terrain"].getAs<irr::scene::ITerrainSceneNode*>();
 		manager->getCore()->getInputManager()->getMouseEvent()->addDelegate(&mouseDelegate);
 		manager->setActive(this,true);
@@ -27,25 +39,29 @@ void ThirdPersonCamera::onAdd()
 
 void ThirdPersonCamera::update(float deltaTime)
 {	
-	if (Mouse[1])//xử lý xoay.
+	if (Mouse[1])//Rotation process.
     {
 		if (!rotating)
 		{
 	       mousePos = cursor->getPosition();
 	       curMousePos = mousePos;
 		   rotating = true;
+
 		}
 		else
 		{
+		   rotation = vector3df(0,15,-25);
 		   mousePos = cursor->getPosition();
-		   rotation.rotateXZBy((mousePos.X - curMousePos.X)*-0.25,vector3df(0,0,0));
-		   //rotation.rotateYZBy((mousePos.Y - curMousePos.Y)*0.25,vector3df(0,0,0));
+		   nRotationX += mousePos.X - curMousePos.X;
+		   nRotationY += mousePos.Y - curMousePos.Y;
+		   rotation.rotateYZBy((nRotationY)*0.25,vector3df(0,0,0));
+		   rotation.rotateXZBy((nRotationX)*-0.25,vector3df(0,0,0));//Hệ số của (mousePos.X - curMousePos.X) quyết định tốc độ và chiều xoay.
 		   curMousePos = mousePos;
 		}
     }
 	    target = targetNode->getPosition();
      	camPos = rotation;
-		camPos *= Zoom;//Xử lý zoom.
+		camPos *= Zoom;//Zoom process.
 		camPos += target;
 	 //Xử lý "độn thổ", "xuyên tường".
         if (camPos.Y < terrain->getHeight(camPos.X,camPos.Z) + 5)
@@ -65,16 +81,16 @@ void ThirdPersonCamera::onRemove()
 
 void ThirdPersonCamera::onMouse(SMouseEvent& args)
 {
-		if(args.mouseEvent==EMIE_MOUSE_WHEEL)//gamer cuộn nút Whell.
+		if(args.mouseEvent==EMIE_MOUSE_WHEEL)//gamer cuộn nút Wheel.
 		{
-			Zoom = Zoom + args.wheel*0.25;//thay tốc độ Zoom = cách chỉnh hệ số wheel.
-			if (Zoom < 0.7)//Nếu gamer nó zoom thấp quá.
-				Zoom = 0.7;//Không cho nó zoom xuống nữa.
+			Zoom = Zoom + args.wheel*0.25;//thay tốc độ Zoom = cách chỉnh hệ số wheel, số càng lớn, zoom càng ác.
+			if (Zoom < 0.5)//Nếu gamer nó zoom thấp quá.
+				Zoom = 0.5;//Không cho nó zoom xuống nữa.
 			if (Zoom > 3.5)//Nếu gamer nó cứ khoái zoom cao.
 				Zoom = 3.5;//Cho cao thế thôi, thằng gamer nào có phản ứng, liên hệ anh NamVoDang.
 		}
 		else if(args.mouseEvent==EMIE_RMOUSE_PRESSED_DOWN)//Phím phải chuột được nhấn.
-		{  
+		{    
 			 Mouse[1] = true;
 		}
 		else if(args.mouseEvent==EMIE_RMOUSE_LEFT_UP)//Phím phải chuột được nhả.
@@ -82,4 +98,9 @@ void ThirdPersonCamera::onMouse(SMouseEvent& args)
 			 Mouse[1] = false;
 			 rotating = false;
 		}
+}
+
+ICameraSceneNode* ThirdPersonCamera::getPointer() const
+{
+	return camera;
 }
--- trunk/Simple Game Framework/D3(with SGF)/ThirdPersonCamera.h
+++ trunk/Simple Game Framework/D3(with SGF)/ThirdPersonCamera.h
@@ -1,7 +1,18 @@
-﻿#ifndef _THIRD_PERSON_CAMERA_H_
+﻿/**
+ * \RunOn: SGF(Simple Game Framework) - Irrlicht Engine
+ * \Summary: Third Person Camera
+ * \Filename: ThirdPersonCamera.h
+ * \Encoding: UTF-8
+ * \CreatedDate: 2:17:PM 2008/06/15
+ * \CreatedBy: FOSP Team - NamVoDang(kiemkhachchilo)
+ * \Copyright: FOS Project
+ **/
+
+#ifndef _THIRD_PERSON_CAMERA_H_
 #define _THIRD_PERSON_CAMERA_H_
 
 #include "SGF.h"
+#include <functional>
 #include <irrlicht.h>
 using namespace irr;
 using namespace scene;
@@ -10,23 +21,23 @@ class ThirdPersonCamera: public sgfEntity //Class ThirdPersonCamera kế thừa 
 {
 public:
 	ThirdPersonCamera(ISceneNode* targetNode);
+	ICameraSceneNode* getPointer() const;//Trả về con trỏ tới CameraSceneNode đang được dùng bởi class.
 private:
-	float Zoom; 
-    s32 Rotation;
+	float Zoom;
+    s32 Rotation,nRotationX,nRotationY;
 	bool rotating;
-	bool Mouse[1];//Trái 0, Phải 1.
+	bool Mouse[1];//Left 0, Right 1.
 	gui::ICursorControl* cursor;
 	position2d<s32> mousePos;
 	position2d<s32> curMousePos;
-	vector3df camPos;//Vị trí đặt camera.
-	vector3df target;//Điểm camera nhìn tới.
+	vector3df camPos;//Camera position.
+	vector3df target;//Look at.
 	vector3df rotation;
 	f32 RotateStartX, RotateStartY, curentRotateStartX, curentRotateStartY;
-	ISceneNode* targetNode;//Nhân vật.
+	ISceneNode* targetNode;//Character.
 	ICameraSceneNode* camera;//Camera.
 	ITerrainSceneNode* terrain;
-	ISceneNode* node;
-	sgfMethodDelegate<ThirdPersonCamera,SMouseEvent> mouseDelegate;//Đăng ký event mouse.
+	sgfMethodDelegate<ThirdPersonCamera,SMouseEvent> mouseDelegate;//Register mouse event.
 	void onAdd();
 	void update(float deltaTime);//Update.
 	void onRemove();
