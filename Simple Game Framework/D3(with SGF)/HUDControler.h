85a797dda0b4140443fc49b58793618cae6e1ac7
doqkhanh <doqkhanh@52f955dd-904d-0410-b1ed-9fe3d3cbfe06>
2008-06-20 23:04:02 +0000

    Add controler entity to communicate minimap vs character

---
+++ Simple Game Framework/D3(with SGF)/HUDControler.h
@@ -0,0 +1,33 @@
+﻿/**
+ * \RunOn: SGF(Simple Game Framework) - Irrlicht Engine
+ * \Summary: Third Person Camera
+ * \Filename: ThirdPersonCamera.h
+ * \Encoding: UTF-8
+ * \CreatedDate: 12:47 AM 6/21/2008
+ * \CreatedBy: FOSP Team 
+ * \Copyright: FOS Project
+ **/
+
+#ifndef _HUD_CONTROLER_H_
+#define _HUD_CONTROLER_H_
+
+#include <SGF.h>
+#include <functional>
+#include <irrlicht.h>
+using namespace irr;
+using namespace scene;
+using namespace core;
+class HUDControler: public sgfEntity
+{
+public:
+	ThirdPersonCamera(ISceneNode* targetNode);
+	
+private:
+	ISceneNode* targetNode;//Character.
+	
+	sgfMethodDelegate<ThirdPersonCamera,SMouseEvent> mouseDelegate;//Register mouse event.
+	void onAdd();
+	void update(float deltaTime);//Update.
+	void onRemove();
+};
+#endif
