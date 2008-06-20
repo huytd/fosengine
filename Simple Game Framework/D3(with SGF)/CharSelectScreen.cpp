1281ef8d660bd37e5613e3c52622136f595f6c81
kingbazoka <kingbazoka@52f955dd-904d-0410-b1ed-9fe3d3cbfe06>
2008-06-20 02:22:06 +0000

    add CharSelectScreen

---
+++ Simple Game Framework/D3(with SGF)/CharSelectScreen.cpp
@@ -0,0 +1,21 @@
+#include "CharSelectScreen.h"
+
+CharSelectScreen::CharSelectScreen()
+{
+
+}
+
+void CharSelectScreen::onEnter(sgfEntityManager *emgr)
+{
+
+}
+
+void CharSelectScreen::onExit(sgfEntityManager *emgr)
+{
+
+}
+
+void CharSelectScreen::onGUIEvent(irr::SEvent::SGUIEvent &args)
+{
+
+}
