4911194c6219c08e2abdf80e4d3612d2793f3879
doqkhanh <doqkhanh@52f955dd-904d-0410-b1ed-9fe3d3cbfe06>
2008-06-21 09:45:29 +0000

    Edit some comment

--- trunk/Simple Game Framework/D3(with SGF)/HealthManaBar.h
+++ trunk/Simple Game Framework/D3(with SGF)/HealthManaBar.h
@@ -6,29 +6,44 @@ using namespace irr;
 using namespace video;
 using namespace core;
 using namespace gui;
-//Định ngĩa class.
-class HealthManaBar : public IGUIElement //Class HealthManaBar kế thừa từ class IGUIElement.
+
+//! Class Define
+class HealthManaBar : public IGUIElement 
 {
 private:
 	IVideoDriver* driver;
-	int maxValue;//Lượng máu lớn nhất.
-	int value;   //Lượng máu hiện tại.
+
+	//! Max status value
+	int maxValue;
+
+	//! Current status value
+	int value; 
+
 	short int transparent;
+
 	ITexture* imgBar;
+
+	//! Bar position
 	position2d<s32> pos;
-	dimension2d<s32> BarSize;//Kích thước bar.
+
+	//! Bar size
+	dimension2d<s32> BarSize;
+
 public:
-	//Khởi tạo đối tượng.
+	//! Default constructor
 	HealthManaBar(const rect<s32>& rect,IGUIEnvironment* env,IGUIElement* parent);
-	//Hủy đối tượng.
+
+	//! Default destructor
 	~HealthManaBar();
-	//Đặt các giá trị mới cho Máu, Mana,V.v....
+
+	//! Set new value
 	void setValue(int Value);
 	void setMaxValue(int MaxValue);
 	void setTexture(c8 *fileName);
 	void setPosition(int X,int Y);
 	void setTransparent(short int Transparent);
-	//Vẽ.
+
+	//Render the bar
 	void draw();
 
 }; 
