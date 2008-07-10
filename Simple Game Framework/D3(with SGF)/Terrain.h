a3bf4a1cdf25d6efee488211079082f40b0e577e
doqkhanh <doqkhanh@52f955dd-904d-0410-b1ed-9fe3d3cbfe06>
2008-07-10 08:37:17 +0000

    TSTerrain code

--- Simple Game Framework/D3(with SGF)/Terrain.h
+++ Simple Game Framework/D3(with SGF)/Terrain.h
@@ -5,6 +5,8 @@
 #include "Demo3.h"
 #include <irrlicht.h>
 
+//#include "TSTerrainSceneNode.h"
+
 using namespace irr;
 using namespace scene;
 using namespace core;
@@ -20,6 +22,32 @@ public:
 
 	void onAdd()
 	{
+		//IrrlichtDevice         *irrDevice         = manager->getCore()->getGraphicDevice();
+		//video::IVideoDriver      *videoDriver      = manager->getCore()->getGraphicDevice()->getVideoDriver();
+		//scene::ISceneManager   *sceneManager      = manager->getCore()->getGraphicDevice()->getSceneManager();
+
+		//// Create TSTerrainSceneNode
+		//TSTerrainSceneNode *terrain = new TSTerrainSceneNode(
+		//		irrDevice->getLogger(),
+		//		sceneManager,
+		//		irrDevice->getTimer(),
+		//		"test03",                     // Mesh Name
+		//		"test03.png",                  // HeightMap Image File Name
+		//		core::dimension2d<f32>(1.0, 1.0),   // Stretch Size            :Same with ISceneManager::addTerrainMesh's parameter
+		//		250.0f,                        // Max Height            :Same with ISceneManager::addTerrainMesh's parameter
+		//		core::dimension2d<s32>(32, 32),      // Default Vertex Block Size:Same with ISceneManager::addTerrainMesh's parameter
+		//		9.39f, 12.85f, 7.947f, 11.478f);   // 1 ~ 4 textures scale      :Same With scene::ITerrainSceneNode::ScaleTexture
+
+		// Set Material Textures In SPECIFIC ORDER
+
+		//terrain->setMaterialTexture(0, videoDriver->getTexture("test03_d.png"));   // Detail Map
+		//terrain->setMaterialTexture(1, videoDriver->getTexture("grayRock.png"));   // 1st Detail Texture
+		//terrain->setMaterialTexture(2, videoDriver->getTexture("hardDirt.png"));   // 2nd Detail Texture
+		//terrain->setMaterialTexture(3, videoDriver->getTexture("bigRockFace.png"));   // 3rd Detail Texture
+		//terrain->setMaterialTexture(4, videoDriver->getTexture("shortGrass.png"));   // 4th Detail Texture
+		//terrain->setMaterialTexture(5, videoDriver->getTexture("test03_l.png"));   // Light Map
+		//terrain->setMaterialTexture(6, videoDriver->getTexture("test03_c.png"));   // Color Map
+
 		manager->getCore()->globalVars["terrain"] = (void*)node;
 
 		manager->getCore()->getGraphicDevice()->getVideoDriver()->setFog(
