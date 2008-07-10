806cee3f5b93d17f1e7e59347198c1a47b1b9595
doqkhanh <doqkhanh@52f955dd-904d-0410-b1ed-9fe3d3cbfe06>
2008-07-10 08:45:54 +0000

    Rename 1 folder

--- Simple Game Framework/D3(with SGF)/D3(with SGF).cpp
+++
@@ -1,7 +0,0 @@
-// D3(with SGF).cpp : Defines the entry point for the console application.
-//
-#include <SGF.h>
-#include "Demo3.h"
-
-//#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
-GAME_ENTRY_POINT(Demo3)
